cc443fc80ea5ee0f32c3296eafd2ab06a9121ed2
1707akash <1707akash@gmail.com>
2024-01-15 23:19:02 +0530

    WT3

---
+++ books.js
@@ -0,0 +1,7 @@
+const productList = [
+    {id: 1, name:   "Eragon"   , price:100},
+    {id: 2, name: "Eldest", price:150},
+    {id: 3, name: "Brisingr", price:300},
+    {id: 4, name: "Inheritance"   , price:750},
+    
+];
